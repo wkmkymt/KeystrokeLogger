2395f75e1973b4bfabb850bc244f6647888bb2be
wkmkymt <wkmkymt@gmail.com>
2018-08-30 16:16:57 +0900

    [add] node moduels

---
+++ statics/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+const pathes = {
+  src:  path.resolve(__dirname, "src"),
+  dist: path.resolve(__dirname, "dist")
+}
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+
+  context: pathes.src,
+  entry: [
+    "./index.jsx"
+  ],
+
+  output: {
+    path: pathes.dist,
+    filename: "./js/keystroke.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: pathes.dist,
+    publicPath: "/",
+    open: true,
+    port: 3000
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+
+};
